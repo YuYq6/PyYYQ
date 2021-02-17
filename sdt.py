89081a93b96d85ac0de53277056d7e9f548dd161
yu yongqing <314589040@qq.com>
2021-02-17 17:37:36 +0800

    修复SDT类：早于1970-1-1的时间

--- sdt.py
+++ sdt.py
@@ -1,4 +1,3 @@
-#! /physical_quantity.py
 # -*- coding: utf-8 -*-
 """
 DateTime    ：2021-02-03
@@ -16,7 +15,7 @@ class SDT:
     """
     实现气象日期串相关内容
     """
-    _length: 14
+    
 
     def __init__(self, **kwargs):
         """
@@ -25,17 +24,15 @@ class SDT:
         """
         print(kwargs)
         if len(kwargs.keys()) == 0:
-            self._dt = time.time()
-            self._sdt = SDT.toSDT(self._dt)
-            self._length = len(self._sdt)
+            self._dt = SDT.toDT()
+            self._length = 14
         elif kwargs.__contains__('sdt'):
             self._sdt = kwargs['sdt']
             self._dt = SDT.toDT(self._sdt)
             self._length = len(self._sdt)
         elif kwargs.__contains__('dt'):
             self._dt = kwargs['dt']
-            self._sdt = SDT.toSDT(self._dt)
-            self._length = len(self._sdt)
+            self._length = 14
         else:
             self._dt = time.time()
             self._sdt = SDT.toSDT(self._dt)
@@ -58,13 +55,14 @@ class SDT:
         """
         :return: 日期串
         """
-        return self._sdt[:self._length]
+        sdt = SDT.toSDT(self._dt)
+        return sdt[:self._length]
 
     @sdt.setter
     def sdt(self, value):
         if str.isdigit(value):
             value += "00000000000000"  # 不足的用0补全
-            self._sdt = value[0:self._length]
+            # self._sdt = value[0:self._length]
             self._dt = SDT.toDT(self._sdt)
         else:
             raise ValueError('sdt must be all digit!')
@@ -76,12 +74,13 @@ class SDT:
     @dt.setter
     def dt(self, value):
         self._dt = value
-        self._sdt = SDT.toSDT(self._dt)
+        # self._sdt = SDT.toSDT(self._dt)
 
     @property
     def zhsdt(self):
         # chinese date
-        d2 = datetime.datetime.strptime(self._sdt, '%Y%m%d%H%M%S')
+        sdt = SDT.toSDT(self._dt)
+        d2 = datetime.datetime.strptime(sdt, '%Y%m%d%H%M%S')
         zhdt = ZhDate.ZhDate.from_datetime(d2)
         if zhdt.leap_month:
             return f"{zhdt.lunar_year}-闰{zhdt.lunar_month}-{zhdt.lunar_day}"
@@ -91,23 +90,29 @@ class SDT:
     @property
     def eraTime(self):
         # ren hours from 1900-1-1 0:0:0
-        dt0 = SDT.toDT("19000101000000")
+        dt0 = SDT.toDT("19000101000000")        
         hours = (self.dt - dt0) / 3600 
         return math.floor(hours)
     
     @property
-    def necpTime(self):
-        return self._sdt[:10]
+    def necpTime(self):        
+        return SDT.toSDT(self._dt)[:10]
     
     # 方法
-    def add(self, seconds):
+    def add(self, seconds, **kwargs):
         """
         计算日期串的
         :param seconds: 秒数
         :return: 14位的日期数字串，
         """
         self._dt += seconds
-        self._sdt = SDT.toSDT(self._dt)
+        if kwargs.__contains__('days'):
+            self._dt += kwargs['days'] * 86400
+        if kwargs.__contains__('hours'):
+            self._dt += kwargs['hours'] * 3600
+        if kwargs.__contains__('minutes'):
+            self._dt += kwargs['minutes'] * 60
+        # self._sdt = SDT.toSDT(self._dt)
         return self
 
     # 静态方法
@@ -118,14 +123,26 @@ class SDT:
         :param args: 时间
         :return: 14位的日期数字串，
         """
+        print('toSDT,args[0]=',args[0])
+        
+        
         if len(args) == 0:
-            return time.strftime("%Y%m%d%H%M%S", time.localtime())
-        return time.strftime("%Y%m%d%H%M%S", time.localtime(args[0]))
+            return time.strftime("%Y%m%d%H%M%S", time.gmtime())
+        else:
+            if args[0] < 0:
+                days = args[0] / 86400
+                days = math.floor(days)
+                seconds = args[0] - days * 86400
+                dt = datetime.datetime(1970,1,1,0,0,0)
+                dt += datetime.timedelta(days=days, seconds=seconds)
+                return dt.strftime("%Y%m%d%H%M%S")
+            else:
+                return time.strftime("%Y%m%d%H%M%S", time.gmtime(args[0]))
 
     @staticmethod
     def toDT(*args):
         """
-        生成时间戳
+        生成时间戳,距离1970-1-1 0：0：0 的秒数
         :param args:
         :return:
         """
@@ -136,12 +153,29 @@ class SDT:
         if str.isdigit(sdt):
             sdt += "00000000000000"
             sdt = sdt[0:14]
-            return time.mktime(time.strptime(sdt, "%Y%m%d%H%M%S"))
+            dt = datetime.datetime.strptime(sdt, "%Y%m%d%H%M%S")
+            dt0 = datetime.datetime.strptime('19700101000000', "%Y%m%d%H%M%S")
+            td = dt - dt0
+            return td.days * 86400 + td.seconds
         else:
             return time.time()
+    
+    @staticmethod
+    def era2necp(eratime):
+        """
+        change era4time:int to necpTime:str("%Y%m%d%H")     
+        """
+        hours_off = -25567
+        hour_dis = eratime + hours_off
+        days = math.floor(hour_dis / 24)
+        seconds = hour_dis * 3600 - days * 86400
+        dt0 = datetime.datetime(1970, 1, 1, 0, 0, 0)
+        dt = dt0 + datetime.timedelta(days=days,seconds=seconds)
+        return dt.strftime('%Y%m%d%H')
 
-
-if "__name__" == 'main':
+if __name__ == '__main__':
+    print('hello world')
     a = SDT()
-    b = SDT(sdt='202102030900')
-    c = SDT(dt=time.time())
+    b = SDT(sdt='19580101215954')
+    c = SDT(dt = time.time())
+    print(c.sdt)
